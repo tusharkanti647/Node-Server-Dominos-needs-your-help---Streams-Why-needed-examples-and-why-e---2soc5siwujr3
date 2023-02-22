7241ec8fc42f2e46a6f66985205f4fc93854d296
tushar <tusharkanti647@gmail.com>
2023-02-22 14:54:20 +0000

    done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
+const obj = {
+    phone: '18602100000',
+    email: 'guestcaredominos@jublfood.com'
+};
 function handleServer(req, res) {
-  
+    if (req.url === "/welcome") {
+        res.end("Welcome to Dominos!");
+    } else if (req.url === "/contact") {
+        const objStr = JSON.stringify(obj);
+        res.end(objStr); //json
+    } else {
+        res.end("bed request");
+        //res.sendStatus(404);
+    }
 }
 
-module.exports = httpServer;
+httpServer.listen(8081);
+
+//module.exports = httpServer;
